327471591a6318a1893b2975336ff10037f731f8
joychineseclass-afk <joychineseclass@gmail.com>
2026-01-22 22:07:13 +0900

    Add word data for business and HSK 1 vocabulary

---
+++ data.js
@@ -0,0 +1,58 @@
+const wordData = {
+    // 【비즈니스 - 실전 사교 (职场社交)】
+    biz: [
+        { 
+            cn: "认识", hj: "認識", py: "rènshi", kr: "런 쓰", trans: "인식/알게되다",
+            sentence_kr: "만나서 반갑습니다 (알게 되어 기쁩니다).",
+            sentence_cn: "认识您很高兴。",
+            sentence_py: "Rènshi nín hěn gāoxìng.",
+            sentence_read: "런쓰 닌 헌 까오싱"
+        },
+        { 
+            cn: "合作", hj: "合作", py: "hézuò", kr: "허 쭈오", trans: "협력/합작",
+            sentence_kr: "함께 일하게 되어 즐겁습니다!",
+            sentence_cn: "合作愉快！",
+            sentence_py: "Hézuò yúkuài!",
+            sentence_read: "허쭈오 위콰이"
+        },
+        { 
+            cn: "辛苦", hj: "辛苦", py: "xīnkǔ", kr: "씬 쿠", trans: "수고/고생",
+            sentence_kr: "모두들 수고하셨습니다.",
+            sentence_cn: "辛苦了，大家。",
+            sentence_py: "Xīnkǔle, dàjiā.",
+            sentence_read: "씬쿠러, 따지아"
+        },
+        { 
+            cn: "关照", hj: "關照", py: "guānzhào", kr: "꽌 쨔오", trans: "보살핌/배려",
+            sentence_kr: "앞으로 잘 부탁드립니다.",
+            sentence_cn: "请多多关照。",
+            sentence_py: "Qǐng duōduō guānzhào.",
+            sentence_read: "칭 뚸뚸 꽌쨔오"
+        },
+        { 
+            cn: "欢迎", hj: "歡迎", py: "huānyíng", kr: "환 잉", trans: "환영",
+            sentence_kr: "저희 회사에 오신 것을 환영합니다.",
+            sentence_cn: "欢迎来到我们公司。",
+            sentence_py: "Huānyíng láidào wǒmen gōngsī.",
+            sentence_read: "환잉 라이따오 워먼 꿍쓰"
+        }
+    ],
+
+    // 【HSK 1급 - 기초 생활 (基础生活)】
+    hsk: [
+        { 
+            cn: "你好", hj: "你好", py: "nǐhǎo", kr: "니 하오", trans: "안녕",
+            sentence_kr: "안녕하세요! 만나서 반가워요.",
+            sentence_cn: "见到你很高兴，你好！",
+            sentence_py: "Jiàndào nǐ hěn gāoxìng, nǐhǎo!",
+            sentence_read: "지엔따오 니 헌 까오싱, 니 하오!"
+        },
+        { 
+            cn: "谢谢", hj: "谢谢", py: "xièxie", kr: "씨에 씨에", trans: "감사",
+            sentence_kr: "도와주셔서 감사합니다.",
+            sentence_cn: "谢谢你的帮助。",
+            sentence_py: "Xièxie nǐ de bāngzhù.",
+            sentence_read: "씨에씨에 니 더 빵쭈"
+        }
+    ]
+};
