67d7273d0ce0f64a90b26634504b7e07d7feba18
joychineseclass-afk <joychineseclass@gmail.com>
2026-02-25 12:46:34 +0900

    Update page.hsk.js

--- ui/pages/page.hsk.js
+++ ui/pages/page.hsk.js
@@ -42,6 +42,12 @@ export async function mount() {
   const verSel = document.getElementById("hskVersion");
   if (verSel) verSel.value = getCurrentVersion();
 
+  // ✅ init UI
+  hskApi = initHSKUI({
+    defaultLevel: 1,
+    autoFocusSearch: false,
+    lang: "ko",
+  });
 
   // ✅ Bind events
   bindHSKEvents();
